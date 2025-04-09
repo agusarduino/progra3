68b1aa64d8e809b65c2bc54a54a927cbbcac521f
sofikuo <skuo@udesa.edu.ar>
2025-04-09 00:09:53 -0300

    hice que el header, los li funcionen como links que lleven a la pagina correspondiente. Tambien hice genero

--- proyecto-integrador/src/components/Header/Navegador.js
+++ proyecto-integrador/src/components/Header/Navegador.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Navegador(props) {
     return (
         <ul className="header">
             {
-                props.barraNavegador.map((elemento, i) => <li key={elemento + i}> {elemento.name}</li>)
-                
+                props.barraNavegador.map((elemento, i) => (
+                    <li key={`${elemento.name}-${i}` }>
+                        <Link to={elemento.path} className="opcionesDelNav">
+                            {elemento.name}
+                        </Link>
+                    </li>
+                ))
             }
             
         </ul>
